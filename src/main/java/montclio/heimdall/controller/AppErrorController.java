a24bdb4332d5a2cad248bcc381a19469ac8a4723
leonardo-dev-br <leonardo.santos@vegus.com.br>
2025-10-01 16:33:17 -0300

    WIP: Adicionando pag de error e tentando para StackOver

---
+++ src/main/java/montclio/heimdall/controller/AppErrorController.java
@@ -0,0 +1,14 @@
+package montclio.heimdall.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+}
